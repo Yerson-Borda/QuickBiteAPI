58cf30f701e9566600075785db16754e341c359f
Yerson-Borda <kira_yerson2407@hotmail.com>
2024-12-05 19:05:22 +0700

    Adding Basket and Order models + status enum

---
+++ DAL/Data/Basket.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Data
+{
+    public class Basket
+    {
+        public Guid Id { get; set; }
+
+        public int Count { get; set; } // Amount of dishes-items in the basket DishInCart
+
+        public Dish Dish { get; set; } 
+
+        public Order? Order { get; set; } 
+
+        public User User { get; set; } 
+    }
+}
---
+++ DTO/BasketDtos/DishBasketDto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace DTO.BasketDtos
+{
+    public class DishBasketDto
+    {
+        [JsonPropertyName("id")]
+        public Guid Id { get; set; }
+        [JsonPropertyName("name")]
+        [Required]
+        public string Name { get; set; }
+        [JsonPropertyName("price")]
+        [Required]
+        public decimal Price { get; set; }
+        [JsonPropertyName("totalPrice")]
+        [Required]
+        public decimal TotalPrice { get; set; }
+        [JsonPropertyName("amount")]
+        [Required]
+        public int Amount { get; set; }
+        [JsonPropertyName("image")]
+        [Required]
+        public string ImageUrl { get; set; }
+    }
+}
